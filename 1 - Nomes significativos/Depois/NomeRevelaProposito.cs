3c19a8bd601b52cd60bb452a2ddf23ead73e29a0
Renato Lima <natenho@gmail.com>
2018-11-21 02:08:08 -0200

    Adjust folders

--- 1 - Nomes significativos/Depois/NomeRevelaProposito.cs
+++ 1 - Nomes significativos/Depois/NomeRevelaProposito.cs
@@ -2,7 +2,7 @@ class NomeRevelaProposito
 {
     public int Calcular(DateTime data)
     {
-        int intervaloDecorridoEmDias = DateTime.Now.Subtract(data).Days;
+        int tempoDecorridoEmDias = DateTime.Now.Subtract(data).Days;
 
         // ...
     }
